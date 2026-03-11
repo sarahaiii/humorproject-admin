482d4da2225d372a3dff94759335749604bffe13
sarahaiii <sarah.ai.2705@gmail.com>
2026-03-11 19:52:57 -0400

    hw7.4

--- app/admin/caption-examples/delete/[id]/route.ts
+++ app/admin/caption-examples/delete/[id]/route.ts
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 
 export async function GET(
     request: Request,
-    { params }: { params: Promise<{ id: string }> }
+    { params }: { params: { id: string }}
 ) {
     const { id } = await params;
     const supabase = await createClient();
